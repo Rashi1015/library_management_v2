90a2b1a424ecd59142c3b99037aa169da232437b
Rashi105 <22f3000442@ds.study.ac.in>
2024-07-25 15:05:51 +0530

    jsfiles

--- static/app.js
+++ static/app.js
@@ -1,6 +1,6 @@
 import router from "./utils/router.js";
 import Navbar from "./components/Navbar.js";
-import store from "./utils/store.js";
+///import store from "./utils/store.js";
 
 new Vue({
   el: "#app",
