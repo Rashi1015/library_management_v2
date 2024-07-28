69feaf9e51ff9ac85ca004e8e85527bc2af21f52
Rashi105 <22f3000442@ds.study.ac.in>
2024-07-28 13:27:44 +0530

    mm

--- application/controllers.py
+++ application/controllers.py
@@ -15,7 +15,41 @@ config = pdfkit.configuration(wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkh
 
 
 
-def create_view():
+def create_views(app):
+    @app.route('/userlogin', methods=['POST'])
+    def user_login():
+        data = request.json
+        username = data.get("username")
+        password = data.get("password")
+        current_user = User.query.filter_by(username=username).first()
+
+        if current_user:
+            if current_user.password == password:
+                session['user_id'] = current_user.id
+                session['username'] = current_user.username
+                session['logged_in'] = True
+                if current_user.role == "admin":
+                    return jsonify({"redirect": "/librariandashboard"})
+                else:
+                    return jsonify({"redirect": "/userdashboard"})
+            else:
+                return jsonify({"error": "Incorrect Password"}), 400
+        else:
+            return jsonify({"error": "User does not Exist"}), 400
+
     @app.route('/')
     def home():
-        return render_template('index.html') # entry point to vue frontend
+        return render_template('index.html')
+
+    @app.route('/profile')
+    @auth_required('token', 'session')
+    def profile():
+        return render_template_string(
+            """
+                <h1> this is homepage </h1>
+                <p> Welcome, {{current_user.email}}</p>
+                <p> Role :  {{current_user.roles[0].description}}</p>
+                <p><a href="/logout">Logout</a></p>
+            """
+        )
+create_views(app)
--- static/components/Navbar.js
+++ static/components/Navbar.js
@@ -1,23 +1,21 @@
-///import router from "../utils/router.js";
-
 const Navbar = {
-    template: `<nav class="navbar navbar-expand-lg bg-body-tertiary">
-                    <div class="container-fluid">
-                      <a class="navbar-brand" href="#">Library Managemnet System</a>
-                      
-                      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                          <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/userlogin">Login</a>
-                          </li> 
-                          <li class="nav-item">
-                            <a class="nav-link" href="/userregister">Register</a>
-                         
-                        </ul>
-                      </div>
-                    </div>
-                  </nav>
-    `
-}
+  template: `
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Library Management System</a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link active" aria-current="page" to="/userlogin">Login</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/userregister">Register</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  `
+};
 
 export default Navbar;
