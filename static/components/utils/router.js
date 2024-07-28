4843f0cf686659db7a91cbdd2c7391a937976168
Rashi105 <22f3000442@ds.study.ac.in>
2024-07-28 12:08:49 +0530

    mm

--- static/components/utils/router.js
+++
@@ -1,9 +0,0 @@
-import user_dashboard from "../pages/user_dashboard.js";
-
-const routes = [
-    {path: "/userdashboard", components: user_dashboard },
-    {path: "/", components: Home.js },
-    
-];
-
-export default router;
