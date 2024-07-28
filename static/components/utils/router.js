98175e848b939671163d85b8cbf6b1983aec3107
22f3000442 <22f3000442@ds.study.iitm.ac.in>
2024-07-28 07:04:09 +0100

    //

--- static/components/utils/router.js
+++ static/components/utils/router.js
@@ -2,6 +2,7 @@ import user_dashboard from "../pages/user_dashboard.js";
 
 const routes = [
     {path: "/userdashboard", components: user_dashboard },
+    {path: "/", components: Home.js },
     
 ];
 
